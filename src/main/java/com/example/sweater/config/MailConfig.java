84ea8332a8770d2a7e588c104c33f66a4daf832e
dasha27atr <dasha27atr@gmail.com>
2019-10-17 20:06:06 +0300

    Second Commit

---
+++ src/main/java/com/example/sweater/config/MailConfig.java
@@ -0,0 +1,59 @@
+package com.example.sweater.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.protocol}")
+    private String protocol;
+
+    @Value("${mail.debug}")
+    private String debug;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private String smtpAuth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private String smtpStartTlsEnable;
+
+    @Value("${spring.mail.properties.mail.smtp.ssl.trust}")
+    private String smtpSslTrust;
+
+    @Bean
+    public JavaMailSender getmailsender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties properties = mailSender.getJavaMailProperties();
+
+        properties.setProperty("mail.transport.protocol", protocol);
+        properties.setProperty("mail.debug", debug);
+        properties.setProperty("mail.smtp.auth", smtpAuth);
+        properties.setProperty("mail.smtp.starttls.enable", smtpStartTlsEnable);
+        properties.put("mail.smtp.ssl.trust", smtpSslTrust);
+
+        return mailSender;
+    }
+}
